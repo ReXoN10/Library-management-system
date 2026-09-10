bfe7d00a34937b62519356734a9d08133f934df7
Mohit Bhuyan <mohitbhuyan95@gmail.com>
2026-09-10 19:34:12 +0530

    Add checkout_book logic with custom exceptions

--- code.py
+++ code.py
@@ -23,12 +23,15 @@ class Member:
 
     def __str__(self):
         return f"{self.name}(ID: {self.member_id})"
+    
 
+class BookNotFoundError(Exception):
+    pass
 
 class BookNotAvailableError(Exception):
     pass
 
-class BookNotFoundError(Exception):
+class MemberNotFoundError(Exception):
     pass
 
 
@@ -55,16 +58,54 @@ class Library:
                 return member
         return None
 
+    def checkout_book(self, isbn, member_id):
+        book=self.find_book(isbn)
+        if book is None:
+            raise BookNotFoundError(f"Book with ISBN {isbn} not found.")
+
+        if book.is_checked_out:
+            raise BookNotAvailableError(f"{book.title} is already checked out.")
+
+        member=self.find_member(member_id)
+        if member is None:
+            raise MemberNotFoundError(f"Member with ID {member_id} not found.")
+
+        book.is_checked_out = True
+        member.borrowed_books.append(book)
+
+
+
 lib = Library()
 lib.add_book(Book("Dune", "Frank Herbert", "12345"))
 lib.add_book(Book("1984", "George Orwell", "17832"))
 lib.add_member(Member("Mohit", "001"))
 lib.add_member(Member("Riya", "002"))
- 
-# print(lib.find_book("12345"))              
 
-# print(lib.find_book("99999"))        
+try:
+    lib.checkout_book("12346", "001")
+    print(lib.find_member("001").borrowed_books)
 
-print(lib.find_member("001"))
+except (MemberNotFoundError, BookNotFoundError, BookNotAvailableError) as e:
+    print(e)
 
-print(lib.find_member("999"))
+try:
+    lib.checkout_book("17832", "002")
+    print(lib.find_member("002").borrowed_books)
+
+except (MemberNotFoundError, BookNotFoundError, BookNotAvailableError) as e:
+    print(e)
+
+
+try:
+    lib.checkout_book("17832", "001")  
+
+except (MemberNotFoundError, BookNotFoundError, BookNotAvailableError) as e:
+    print(e)
+
+
+try:
+    lib.checkout_book("12345", "003")
+    print(lib.find_member("003").borrowed_books)
+
+except (MemberNotFoundError, BookNotFoundError, BookNotAvailableError) as e:
+    print(e)
